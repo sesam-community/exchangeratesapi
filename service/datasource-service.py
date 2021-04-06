ab97c8e3117edf3076507adcb2c95c424f0cda50
Baard H. Rehn Johansen <baard.johansen@bouvet.no>
2021-04-06 21:04:25 +0200

    Initial commit

---
+++ service/datasource-service.py
@@ -0,0 +1,79 @@
+
+from flask import Flask, request, Response
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import os
+
+import json
+import pytz
+import iso8601
+import requests
+import logging
+
+app = Flask(__name__)
+
+logger = None
+
+base_url = "https://api.exchangeratesapi.io/"
+
+def datetime_format(dt):
+    return '%04d' % dt.year + dt.strftime("-%m-%dT%H:%M:%SZ")
+
+
+def to_transit_datetime(dt_int):
+    return "~t" + datetime_format(dt_int)
+
+
+def get_var(var, default = None):
+    envvar = default
+    if var.upper() in os.environ:
+        envvar = os.environ[var.upper()]
+    elif request:
+        envvar = request.args.get(var)
+    logger.debug("Setting %s = %s" % (var, envvar))
+    return envvar
+
+
+@app.route('/', methods=['GET'])
+def get_entities():
+    since = get_var('since') or "1999-01-04"
+    base = get_var('base') or "EUR"
+    symbols = get_var('symbols') or ""
+
+
+    entities = []
+
+    end = datetime.now(pytz.UTC).date()  # we need to use UTC as salesforce API requires this
+
+
+    start = iso8601.parse_date(since).date()
+
+    while start <= datetime.now(pytz.UTC).date():
+        response = requests.get("%s%s?base=%s&symbols=%s" % (base_url, start,base,symbols))
+        result = response.json()
+
+        result.update({"_id": "%s-%s" % (base, start)})
+        result.update({"_updated": "%s" % start})
+        result.update({"date": "%s" % to_transit_datetime(iso8601.parse_date(result["date"]))})
+        entities.append(result)
+
+        start = (start + relativedelta(days=1))
+
+    return Response(json.dumps(entities), mimetype='application/json')
+
+
+
+if __name__ == '__main__':
+    # Set up logging
+    format_string = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logger = logging.getLogger('azure-billing-microservice')
+
+    # Log to stdout
+    stdout_handler = logging.StreamHandler()
+    stdout_handler.setFormatter(logging.Formatter(format_string))
+    logger.addHandler(stdout_handler)
+
+    logger.setLevel(logging.INFO)
+
+    app.run(debug=False, host='0.0.0.0', port=5000)
+
